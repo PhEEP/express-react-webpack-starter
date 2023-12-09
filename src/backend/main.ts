d87897f01531e7c67a1c21a3c1cad1e1175a634e
Philippe Moore <philippealmoore@gmail.com>
2023-12-09 10:01:45 -0800

    Refactor main.ts file to use ES6 syntax and add an endpoint to initialize file explorer

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,18 +1,19 @@
-import * as express from 'express';
-import * as path from 'path';
+import * as express from "express"
+import * as path from "path"
+import { files } from "./data/files"
 
-var app = express();
+var app = express()
 
 // Simple endpoint that returns the current time
-app.get('/api/time', function(req, res) {
-    res.send(new Date().toISOString());
-});
+app.get("/api/initializeFileExplorer", function (req, res) {
+  res.send(JSON.stringify(files))
+})
 
 // Serve static files
-app.use('/', express.static(path.join(__dirname, '/www')));
+app.use("/", express.static(path.join(__dirname, "/www")))
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+  console.log(`Server listening on port ${PORT}`)
+})
