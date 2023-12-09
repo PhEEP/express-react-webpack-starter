3cdd136204f57874018d3e9fcbaab4d6fb733bb7
Philippe Moore <philippealmoore@gmail.com>
2023-12-09 09:36:19 -0800

    Scaffold file explorer component and add initial API call
    TODO, add API endpoint to return JSON of directory structure

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,45 +1,57 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import * as React from "react"
+import { createRoot } from "react-dom/client"
 
 interface AppProps {
-    name: string;
+  name: string
+}
+
+type FileItem = {
+  id: number
+  isFolder: boolean
+  items: FileItem[]
+  name: string
 }
 
 interface AppState {
-    time: string;
+  fileExplorer: FileItem[] | null
 }
 
 export class App extends React.Component<AppProps, AppState> {
-
-    constructor(props: AppProps) {
-        super(props);
-        this.state = {
-            time: null
-        }
+  constructor(props: AppProps) {
+    super(props)
+    this.state = {
+      fileExplorer: null,
     }
-    
-    componentDidMount() {
-        this.getTime();
-        setInterval(this.getTime, 2000);
-    }
+  }
 
-    render() {
-        const {name} = this.props;
-        const {time} = this.state;
-        return <><h1>{name}</h1><div>{time}</div></>;
-    }
+  componentDidMount() {
+    this.getTime()
+    setInterval(this.getTime, 2000)
+  }
 
-    private getTime = async () => {
-        const response = await fetch('/api/time', { method: 'GET' });
-        if (response.ok) {
-            this.setState({time: await response.text()});
-        }
-    }
+  render() {
+    const { name } = this.props
+    const { fileExplorer } = this.state
+    return (
+      <>
+        <h1>{name}</h1>
+        <pre>{JSON.stringify(fileExplorer, null, 2)}</pre>
+      </>
+    )
+  }
 
+  private getTime = async () => {
+    const response = await fetch("/api/initializeFileExplorer", {
+      method: "GET",
+    })
+    if (response.ok) {
+      this.setState({ fileExplorer: await response.json() })
+    }
+  }
 }
 
 export function start() {
-    const rootElem = document.getElementById('main');
-    const root = createRoot(rootElem);
-    root.render(<App name="Hello World" />);
+  const rootElem = document.getElementById("main")
+  const root = createRoot(rootElem)
+  root.render(<App name="Philippe's Phile Explorer" />)
 }
