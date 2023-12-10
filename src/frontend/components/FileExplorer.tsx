b31bbfeea25ce5bcddbfe8715692378af8fddd1d
Philippe Moore <philippealmoore@gmail.com>
2023-12-10 12:48:16 -0800

    Refactor file creation logic and add file extension validation

--- src/frontend/components/FileExplorer.tsx
+++ src/frontend/components/FileExplorer.tsx
@@ -1,14 +1,11 @@
 import React, { useState } from "react"
 import { FileItem } from "../App"
-import FileIcons, { FileType } from "./FileIcons"
+import FileIcons from "./FileIcons"
+import { isFileType, type FileType } from "../types/files"
 
 interface FileExplorerProps {
   fileSystem: FileItem
-  handleInsertNode: (
-    fileExplorer: FileItem,
-    folderId: FileItem["id"],
-    payload: FileItem
-  ) => void
+  handleInsertNode: (folderId: FileItem["id"], payload: FileItem) => void
 }
 
 export const FileExplorer: React.FC<FileExplorerProps> = ({
@@ -16,50 +13,61 @@ export const FileExplorer: React.FC<FileExplorerProps> = ({
   handleInsertNode,
 }: FileExplorerProps) => {
   const [isExpanded, setIsExpanded] = useState(false)
-  const [selectedItem, setSelectedItem] = useState<FileItem | null>(null)
+  const [selectedItem, setSelectedItem] = useState(false)
+  const [error, setError] = useState("")
   const [addingNewItem, setAddingNewItem] = useState({
     isFolder: false,
     visible: false,
   })
 
-  // detangle this mess
   const handleNewItem = (
     e: React.MouseEvent<HTMLButtonElement>,
     isFolder: boolean
   ) => {
     e.stopPropagation()
-    setIsExpanded(true)
-    if (isFolder) {
-      setAddingNewItem({ isFolder: true, visible: true })
-    }
-    if (!isFolder) {
-      console.log("Creating new file in: ", fileSystem.id)
-      setAddingNewItem({ isFolder: false, visible: true })
-    }
+    setAddingNewItem({ isFolder, visible: !addingNewItem.visible })
   }
 
   // this is a form submission handler
   const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setError("")
     const form = e.currentTarget
     const formData = new FormData(form)
-    const fileItem = formData.get("fileItem") as string
-    console.log(fileItem, "isFolder: ", addingNewItem.isFolder)
+    const fileName = formData.get("fileItem") as string
+    // if (!fileName) return
+
+    const fileExtension = fileName?.split(".")[1]?.toLowerCase()
+    console.log(
+      "🚀 ~ file: FileExplorer.tsx:39 ~ handleSubmit ~ fileExtension:",
+      fileExtension
+    )
+
+    // rudimentary file extension validation
+    if (
+      (!addingNewItem.isFolder && !fileExtension) ||
+      (fileExtension && !isFileType(fileExtension))
+    ) {
+      setError("Invalid file extension")
+      return
+    }
+
+    const fileItem: FileItem = {
+      id: Date.now(),
+      isFolder: addingNewItem.isFolder,
+      name: fileName,
+      items: addingNewItem.isFolder ? [] : undefined,
+    }
+    handleInsertNode(fileSystem.id, fileItem)
+    setAddingNewItem({ ...addingNewItem, visible: false })
   }
 
   // TODO lift the selected items to global state via context to support multiple selection
   // Or maybe just listen to some dispatch event that will update the selected items since
   // they are all managing their own selected states?
-  const handleSelectItem = (
-    e: React.MouseEvent<HTMLDivElement>,
-    fileSystem: FileItem
-  ) => {
+  const handleSelectItem = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation()
-    if (selectedItem?.id === fileSystem.id) {
-      setSelectedItem(null)
-    } else {
-      setSelectedItem(fileSystem)
-    }
+    setSelectedItem(!selectedItem)
   }
 
   //
@@ -76,13 +84,11 @@ export const FileExplorer: React.FC<FileExplorerProps> = ({
       <div className='directory'>
         <div
           style={{
-            ...(selectedItem?.id === fileSystem.id
-              ? { background: "lightGrey" }
-              : null),
+            ...(selectedItem ? { background: "lightGrey" } : null),
           }}
           className='folder'
           onClick={(e) => {
-            handleSelectItem(e, fileSystem)
+            handleSelectItem(e)
           }}
         >
           <span>
@@ -93,11 +99,15 @@ export const FileExplorer: React.FC<FileExplorerProps> = ({
             <button onClick={() => setIsExpanded(!isExpanded)}>
               {isExpanded ? "Collapse" : "Expand"}
             </button>
-            <button onClick={(e) => handleNewItem(e, true)}>➕📁</button>
-            <button onClick={(e) => handleNewItem(e, false)}>➕📄</button>
-            <button onClick={(e) => handleDeleteItem(e, fileSystem.id)}>
-              🗑️
-            </button>
+            {selectedItem ? (
+              <>
+                <button onClick={(e) => handleNewItem(e, true)}>➕📁</button>
+                <button onClick={(e) => handleNewItem(e, false)}>➕📄</button>
+                <button onClick={(e) => handleDeleteItem(e, fileSystem.id)}>
+                  🗑️
+                </button>
+              </>
+            ) : null}
           </div>
         </div>
         {addingNewItem.visible && (
@@ -116,14 +126,15 @@ export const FileExplorer: React.FC<FileExplorerProps> = ({
                   autoFocus
                   // I'm sorry, I'm so sorry, I just wanted the form submission to work and retain the input disappearing on blur...
                   // Please forgive me 🙏🏻
-                  onBlur={() =>
-                    setInterval(() => {
-                      setAddingNewItem({ ...addingNewItem, visible: false })
-                    }, 100)
-                  }
+                  // onBlur={() =>
+                  //   setInterval(() => {
+                  //     !addingNewItem.setAddingNewItem({ ...addingNewItem, visible: false })
+                  //   }, 500)
+                  // }
                   required
                   aria-required
                 />
+                {error ? <span>{error}</span> : null}
               </label>
               <button type='submit'>Save</button>
             </form>
