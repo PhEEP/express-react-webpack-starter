7c1c1b9f1680b3a6a831a5cfcffa1616b97afa66
Philippe Moore <philippealmoore@gmail.com>
2023-12-10 07:00:11 -0800

    Refactor file explorer initialization and add loading state

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { createRoot } from "react-dom/client"
 import { FileExplorer } from "./components/FileExplorer"
 import "./styles.css"
@@ -19,6 +19,7 @@ export type FileItem = {
 
 export function App({ name }: AppProps) {
   const [fileExplorer, setFileExplorer] = useState<FileItem | null>(null)
+  const [loading, setLoading] = useState(false)
   const { insertNode } = useTreeWalker()
 
   const handleInsertNode = (
@@ -29,30 +30,56 @@ export function App({ name }: AppProps) {
     const finalTree = insertNode(fileExplorer, folderId, payload)
     setFileExplorer(finalTree)
   }
-  useEffect(() => {
-    initFileExplorer()
-  }, [])
+
+  // useEffect(() => {
+  //   initFileExplorer()
+  // }, [])
 
   const initFileExplorer = async () => {
-    const response = await fetch("/api/initializeFileExplorer", {
-      method: "GET",
-    })
-    if (response.ok) {
-      setFileExplorer(await response.json())
+    try {
+      // Simulate network request delay
+      setLoading(true)
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      const response = await fetch("/api/initializeFileExplorer", {
+        method: "GET",
+      })
+
+      if (response.ok) {
+        setFileExplorer(await response.json())
+      } else {
+        throw new Error("Failed to initialize file explorer")
+      }
+    } catch (error) {
+      console.error(error)
+      // Handle error here
+    } finally {
+      // Set loading state to false
+      setLoading(false)
     }
   }
 
   return (
     <>
       <h1>{name}</h1>
-      <button onClick={initFileExplorer}>Demo</button>
-      {fileExplorer && (
-        <FileExplorer
-          fileSystem={fileExplorer}
-          handleInsertNode={handleInsertNode}
-        />
+      <button onClick={() => initFileExplorer()}>Demo</button>
+      {loading ? (
+        <div>
+          <h2>Fetching files</h2>
+          {/* TODO actual spinner for loading state */}
+          <span className='spinner'>🦔</span>
+        </div>
+      ) : (
+        <>
+          {fileExplorer && (
+            <FileExplorer
+              fileSystem={fileExplorer}
+              handleInsertNode={handleInsertNode}
+            />
+          )}
+          <pre>{JSON.stringify(fileExplorer, null, 2)}</pre>
+        </>
       )}
-      <pre>{JSON.stringify(fileExplorer, null, 2)}</pre>
     </>
   )
 }
