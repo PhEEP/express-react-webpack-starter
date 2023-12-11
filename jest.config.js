ecb284c26ce08876447aa08c44de2d7f49770c65
Philippe Moore <philippealmoore@gmail.com>
2023-12-10 20:12:54 -0800

    Add Jest setup file and update Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   roots: ["<rootDir>/src/frontend"],
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
   moduleNameMapper: {
     "\\.(css|less|scss|sss|styl)$": "identity-obj-proxy",
   },
